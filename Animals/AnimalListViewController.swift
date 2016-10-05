62f312ccf6cea25c1cf0e5691ddab4b283f37e79
Ben Su <bensu1013@hotmail.com>
2016-10-05 14:33:16 -0400

    Dogs and Music Jim, Not cats? Gasp

--- Animals/AnimalListViewController.swift
+++ Animals/AnimalListViewController.swift
@@ -9,4 +9,53 @@
 import UIKit
 
 class AnimalListViewController: UITableViewController {
+    
+    var dogArray: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dogArray = [
+            "Bethoveen",
+            "Mozart",
+            "Bach",
+            "Chopin",
+            "Amadeus",
+            "Wolfgang",
+            "Toto",
+            "Spot"
+        ]
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dogArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DogCell", for: indexPath)
+        cell.textLabel?.text = dogArray[indexPath.row]
+        
+        
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier != "DetailsSegue" {return}
+        if let nextView = segue.destination as? AnimalViewController,
+            let index = tableView.indexPathForSelectedRow{
+            nextView.dogString = dogArray[index.row]
+            print(dogArray[index.row])
+        }
+       
+        
+        
+        
+    }
+    
+    
+    
 }
--- Animals/AnimalViewController.swift
+++ Animals/AnimalViewController.swift
@@ -9,4 +9,45 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
+    
+    @IBOutlet weak var EmojiLabel: UILabel!
+    
+    @IBOutlet weak var TextLabel: UILabel!
+    
+    var dogString: String = ""
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        EmojiLabel.text = getEmojiFromString()
+        TextLabel.text = dogString
+        
+    }
+    
+    func getEmojiFromString() -> String {
+        switch dogString {
+        case "Bethoveen":
+           return "🦀"
+        case "Mozart":
+            return "🐍"
+        case "Bach":
+            return "🐢"
+        case "Chopin":
+            return "🐺"
+        case "Amadeus":
+            return "🐗"
+        case "Wolfgang":
+            return "🦄"
+        case "Toto":
+            return "🐝"
+        case "Spot":
+            return "🐛"
+        default:
+            return "🐺"
+        }
+        
+        
+    }
+    
 }
